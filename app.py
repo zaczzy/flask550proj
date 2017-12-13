86365b468e6fa661acd783da083de5c51fab2614
Zack <zhaozeyu@seas.upenn.edu>
2017-12-13 03:22:14 -0500

    fixed some bugs

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/result',methods=['POST'])
+
+
+@app.route('/result', methods=['POST'])
 def search():
     firstLike = request.values.get("firstLike")
     secondLike = request.values.get("secondLike")
@@ -13,6 +16,9 @@ def search():
     directorLike = request.form['directorLike']
     actorDislike = request.form['actorDislike']
     directorDislike = request.form['directorDislike']
-    return '%s +  %s  + %s + %s + %s + %s' % (firstLike, secondLike, actorLike, directorLike, actorDislike, directorDislike)
+    return '%s +  %s  + %s + %s + %s + %s' % (
+    firstLike, secondLike, actorLike, directorLike, actorDislike, directorDislike)
+
+
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 8080)
+    app.run(host='192.168.1.131', port=8080)
